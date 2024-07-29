8664a4e1e02276a593f366a8a6062317fa45746a
Amr Ibrahim <amramr3434@gmail.com>
2024-07-29 19:30:16 +0300

    previos shit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import commonjs from '@rollup/plugin-commonjs'
 
 export default defineConfig({
-  plugins: [vue(), commonjs()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
@@ -15,7 +14,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     minify: 'terser',
-    chunkSizeWarningLimit: 1000,
     emptyOutDir: true,
     terserOptions: {
       compress: {
@@ -27,11 +25,10 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+            return 'vendor'
           }
         }
-      },
-      external: [/\.eslintrc\.cjs$/]
+      }
     }
   },
 
